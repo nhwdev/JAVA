703e957a01cc8a96d8e8fba51c9852d64ee1e940
shtu0 <shtu0@DESKTOP-QHA1IK6>
2026-02-03 08:53:45 +0900

    https://www.acmicpc.net/problem/27866

---
+++ codingTest/src/boj/boj_27866.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+// https://www.acmicpc.net/problem/27866
+
+public class boj_27866 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str = br.readLine();
+		char[] chars = new char[str.length()];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int index = Integer.parseInt(st.nextToken());
+		for(int i =0; i < str.length(); i++) {
+			chars[i] = str.charAt(i);
+			if((index-1) == i){
+				bw.write(chars[i]);
+				break;
+			}
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
