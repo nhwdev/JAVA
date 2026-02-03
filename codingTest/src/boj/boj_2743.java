6b84ad8f3bb43c770e6382191d0e46fedc08e2cc
shtu0 <shtu0@DESKTOP-QHA1IK6>
2026-02-03 09:22:58 +0900

    https://www.acmicpc.net/problem/2743

--- codingTest/src/boj/boj_2743.java
+++ codingTest/src/boj/boj_2743.java
@@ -11,7 +11,7 @@ public class boj_2743 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(br.readLine().length()+"");
+		bw.write(String.valueOf(br.readLine().length()));
 		bw.flush();
 		bw.close();
 		br.close();
