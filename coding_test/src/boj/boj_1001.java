25e6836808db75a3398c93378b5c52dde5fccc60
nhwdev <nhw.dev@gmail.com>
2026-02-07 15:00:27 +0900

    https://www.acmicpc.net/problem/1001

--- coding_test/src/boj/boj_1001.java
+++ coding_test/src/boj/boj_1001.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+// https://www.acmicpc.net/problem/1001
 public class boj_1001 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
